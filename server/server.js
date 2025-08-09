3e1de898f67b4791fd342a9559795008fec374b7
Sunil Baghel <sunilbaghel93100@gmail.com>
2025-08-09 18:28:28 +0530

    server added

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.end("Hello");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
