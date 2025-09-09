f436346d4e534ded3f6af45cdde0263784cd0d69
Sunil Baghel <sunilbaghel93100@gmail.com>
2025-09-09 21:58:13 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import BloodChainLanding from './pages/landing';
 import BloodManagementSystem from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import FutureScopes from './pages/FutureScopes'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/dashboard" element={<BloodManagementSystem />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/future-scopes" element={<FutureScopes />} />
       </Routes>
     </Router>
   );
